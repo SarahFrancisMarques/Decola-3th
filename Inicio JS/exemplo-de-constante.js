6b3b7bb5f1df976456b0ab3b8df513cb299887cc
SarahFrancisMarques <75794109+SarahFrancisMarques@users.noreply.github.com>
2022-06-25 21:41:59 -0300

    segundo commit

---
+++ Inicio JS/exemplo-de-constante.js
@@ -0,0 +1,5 @@
+// var preco = 2;
+
+var desc = 0.2;
+const PRECO = 2;
+ var total = PRECO - desc;
---
+++ Inicio JS/teste.js
@@ -0,0 +1,8 @@
+//comentario de uma linha
+/* inicio
+fim */
+
+var y = 0;
+var x = 1;
+var z = 2;
+
