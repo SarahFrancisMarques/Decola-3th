6df13689227ed37367c2b4103c276df2acf5264d
SarahFrancisMarques <75794109+SarahFrancisMarques@users.noreply.github.com>
2022-06-26 20:54:09 -0300

    sexto commit

---
+++ Sintaxe basica em javascript/aula 5/script.js
@@ -0,0 +1,18 @@
+funções - funções
+
+como declarar
+deve ser declarada usando a função palavra seguida de "()" e "{}"
+/* funação funcao() {
+    console.log('mensagem de uma função');
+}
+chamando a função
+funcao(); */
+
+/* 
+função com parâmetros
+definindo parâmetros
+mensagem de função (primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+chamando a função atribuindo valor aos parâmetros
+mensagem('Tudo certo', 'jovem!'); */
