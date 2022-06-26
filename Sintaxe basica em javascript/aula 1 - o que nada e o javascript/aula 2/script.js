c3e6d2e0eca3ace7ab8c2665638cbd0f6447eb5b
SarahFrancisMarques <75794109+SarahFrancisMarques@users.noreply.github.com>
2022-06-25 23:41:33 -0300

    quinto commit

---
+++ Sintaxe basica em javascript/aula 1 - o que nada e o javascript/aula 2/script.js
@@ -0,0 +1,94 @@
+// tipos primitivos
+
+// boolean
+/*var VouF = false;
+console.log(typeof(VouF));
+
+//number
+var numeroqual = 1;
+console.log(typeof(numeroqual));
+
+//string
+var nome = 'SARAH';
+console.log(typeof(nome));
+
+//function
+var funcao = function(){}
+console.log(typeof(funcao)); */
+
+//como declarar
+/*var variavel = 'SARAH';
+variavel = 'ana';
+console.log(variavel);*/
+
+/* let variavel2 = 'SARAH';
+variavel2 = 'ana';
+console.log(variavel2);*/
+
+/*const constante = 'SARAH';
+constante = 'ana';
+console.log(constante);*/
+
+
+
+
+
+        /* // atribuição
+        var atribuicao = 'diana';
+        // comparação
+        var comparacao = "0" == 0;
+        console.log(comparacao);
+        // comparação identica
+        var comparacaoIdentica = "0" === 0;
+        console.log(comparacaoIdentica); */
+
+
+       /*  // operadores aritméticos
+        // adição
+        var adicao = 1 + 1;
+        console.log(adicao);
+        //subtração
+        var subtracao = 2 - 1;
+        console.log(subtracao);
+        //multiplicação
+        var multiplicacao = 2 * 3;
+        console.log(multiplicacao);
+        //divisão real
+        var divisaoReal = 6 / 2;
+        console.log(divisaoReal);
+        //divisão inteira
+        var divisaoInteira = 5 % 2;
+        console.log(divisaoInteira);;
+        //potenciação
+        var potenciacao = 2**10;
+        console.log(potenciacao); */
+
+
+       /*  // operadores relacionais
+        // maior que
+        var maiorQue = 5 > 2;
+        console.log(maiorQue);
+        
+        // menor que
+        var menorQue = 5 < 2;
+        console.log(menorQue);
+        
+        // maior ou igual a
+        var maiorOuIgual = 5 >= 2;
+        console.log(maiorOuIgual);
+        
+        // menor ou igual a
+        var menorOuIgual = 5 <= 2;
+        console.log(menorOuIgual); */
+
+
+       /*  // operadores lógicos
+        // && - todos os valores devem ser true
+        var e = true && true;
+        console.log(e);
+        // || - somente um valor deve ser true
+        var ou = true || false;
+        console.log(ou);
+        // ! - inverte os valores
+        var nao = !true;
+        console.log(nao); */
