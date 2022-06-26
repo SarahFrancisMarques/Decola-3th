7ac7107d8a9b0a64aa4399d2ccfe32eaa0f03bf7
SarahFrancisMarques <75794109+SarahFrancisMarques@users.noreply.github.com>
2022-06-25 22:47:27 -0300

    quarto commit

--- Exemplo projeto de estrutura/assets/assets/js/scripts.js
+++ Exemplo projeto de estrutura/assets/assets/js/scripts.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+var currentNumberwrapper = document.getElementById('currentNumeber');
+var currentNumber = 0;
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberwrapper.innerHTML = currentNumber;
+}
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberwrapper.innerHTML = currentNumber;
+}
